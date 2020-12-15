7ce69213bf3ca51957069596ac273392ff461c94
Vignesh <vignesh.ramasamyy@gmail.com>
2020-12-15 11:13:48 +0530

    one commit

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,15 +1,9 @@
 <template>
   <div class="header-style">
     <div>
-      <img
-        src="../assets/settings.svg"
-        alt=""
-        style="width: 25px; height: auto; fill: #CCD1D5;"
-      />
+      <img src="../assets/settings.svg" alt="" style="width: 25px; height: auto; fill: #CCD1D5;" />
     </div>
-    <div
-      style="text-align: center;font-family: 'Pacifico', cursive; font-size: 24px; font-weight: 400; flex-grow: 1;"
-    >
+    <div style="text-align: center;font-family: 'Pacifico', cursive; font-size: 24px; font-weight: 400; flex-grow: 1;">
       Instagram
     </div>
     <div class="mail-icon">
@@ -21,31 +15,30 @@
 </template>
 
 <script>
-export default {
-  name: "Header",
-};
+  export default {
+    name: "Header",
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.header-style {
-  @media (max-width: 576px) {
-    display: flex;
-    //justify-content: space-around;
-    align-items: center;
-    padding: 2px 0 3px 0;
-    margin-left: 1.5rem;
-    margin-right: 1.5rem;
+  .header-style {
+    @media (max-width: 576px) {
+      display: flex;
+      align-items: center;
+      padding: 2px 0 3px 0;
+      margin-left: 1.5rem;
+      margin-right: 1.5rem;
+    }
+
+    .noti {
+      position: absolute;
+      width: 8px;
+      height: 8px;
+      background-color: #fc045c;
+      border-radius: 50px;
+      right: 21px;
+      top: 24px;
+    }
   }
-  .noti {
-    position: absolute;
-    width: 8px;
-    height: 8px;
-    background-color: #fc045c;
-    border-radius: 50px;
-    right: 21px;
-    top: 24px;
-    //hide
-  }
-}
 </style>
